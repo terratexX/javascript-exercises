01e46c3ad3bb7d020f9e760054c6b484243b3d7a
terratexX <tomidevil@hotmail.com>
2022-09-30 16:48:05 +0200

    Code exercise 01-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...numbers) {
+  return array.filter((item) => !numbers.includes(item));
 };
 
 // Do not edit below this line
