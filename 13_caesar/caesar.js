117955c4ecad95ed2d02dd37cf19a662d8417477
terratexX <tomidevil@hotmail.com>
2022-10-04 00:30:33 +0200

    Solve exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -3,25 +3,59 @@ const caesar = function (string, num) {
   for (let i = 0; i < string.length; i++) {
     let letter = string[i];
     let letterNum = string.charCodeAt(i);
-    if (letter === letter.toUpperCase() && letterNum + num < 65) {
-      output += string[i].replace(
-        /\w/g,
-        String.fromCharCode(91 - (letterNum + -num - 65))
-      );
-    } else if (letter === letter.toUpperCase() && letterNum + num > 90) {
-      output += string[i].replace(
-        /\w/g,
-        String.fromCharCode(64 + (letterNum + num - 90))
-      );
-    } else {
+
+    if (letter === letter.toUpperCase() && letterNum + num > 90) {
+      let count = letterNum + num - 90;
+      let startNum = 64;
+      while (count > 0) {
+        if (startNum == 90) {
+          startNum = 64;
+        }
+        startNum += 1;
+        count--;
+      }
+      output += string[i].replace(/\w/g, String.fromCharCode(startNum));
+    } else if (letter === letter.toUpperCase() && letterNum + num < 65) {
+      let count = 65 - (letterNum + num);
+      let startNum = 91;
+      while (count > 0) {
+        if (startNum == 65) {
+          startNum = 91;
+        }
+        startNum -= 1;
+        count--;
+      }
+      output += string[i].replace(/\w/g, String.fromCharCode(startNum));
+    } else if (letter === letter.toUpperCase()) {
+      output += string[i].replace(/\w/g, String.fromCharCode(letterNum + num));
+    } else if (letter === letter.toLowerCase() && letterNum + num > 122) {
+      let count = letterNum + num - 122;
+      let startNum = 96;
+      while (count > 0) {
+        if (startNum == 122) {
+          startNum = 96;
+        }
+        startNum += 1;
+        count--;
+      }
+      output += string[i].replace(/\w/g, String.fromCharCode(startNum));
+    } else if (letter === letter.toLowerCase() && letterNum + num < 97) {
+      let count = 97 - (letterNum + num);
+      let startNum = 123;
+      while (count > 0) {
+        if (startNum == 97) {
+          startNum = 123;
+        }
+        startNum -= 1;
+        count--;
+      }
+      output += string[i].replace(/\w/g, String.fromCharCode(startNum));
+    } else if (letter === letter.toLowerCase()) {
       output += string[i].replace(/\w/g, String.fromCharCode(letterNum + num));
     }
-    console.log(letterNum);
-    console.log(letterNum + -num);
   }
   return output;
 };
-console.log(caesar("B", -2));
 
 //todo: "a"=97 to "z"=122,   "A"=65 to "Z"=90
 
